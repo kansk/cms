9ab72eec621ffdafed64b32ccac83193a7f70c32
kansk <32514741+kansk@users.noreply.github.com>
2017-12-12 22:54:13 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-export http_proxy=http://165.225.104.34:80
-export https_proxy=http://165.225.104.34:80
-
-pip install -r /opt/llo/cms/requirements.txt
-
-python /opt/llo/cms/cms.py 0.0.0.0 app_catalog cloudlet_catalog microservice_catalog
